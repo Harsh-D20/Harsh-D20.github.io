ed3a925b16dad54f160cd3bcd69ffc16dd9019a1
Harsh <harshdesai906@gmail.com>
2024-10-02 22:38:40 -0400

    undo test

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -7,7 +7,6 @@ function Header() {
                 <img alt="logo" className={styles.logo} src="/logo.png"></img>
                 <h1 id={styles["title"]}>Harsh Desai</h1>
                 <nav className={styles.navbar}>
-                    <p>Test</p>
                     {/* <ul className={styles.options}>
                         <li><a href="#">Home</a></li>
                     </ul> */}
